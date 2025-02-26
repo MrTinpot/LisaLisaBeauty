6ab26e067a11e2adc81a66865c4fc3c0a50ed8cf
Tinpot <147400116+MrTinpot@users.noreply.github.com>
2025-02-27 01:19:44 +0300

    Add project files.

---
+++ LisaLisaBeauty/Controllers/Kategori.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LisaLisaBeauty.Controllers
+{
+    public class Kategori : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
